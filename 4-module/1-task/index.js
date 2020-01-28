f3aa8443a8189e3a0f7e7e70c7523298d8f42f4e
Denis Chizh <chizhds@sibur.ru>
2020-01-28 20:46:59 +0300

    Занятие 4. Задача 1

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,13 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+  let ulElem = document.createElement('ul');
+  
+  let html = '';
+  for (let friend of friends) {
+    html += `<li>${friend.firstName} ${friend.lastName}</li>`;
+  }
+  ulElem.innerHTML = html;
+  
+  return ulElem;
 }
