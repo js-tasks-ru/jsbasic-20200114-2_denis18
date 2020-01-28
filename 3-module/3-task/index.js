368b3674d14b5114cd75bc35db7188b829db05cd
Denis Chizh <chizhds@sibur.ru>
2020-01-28 21:11:28 +0300

    Занятие 3. Задача 3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,4 +3,17 @@
  * @returns {string}
  */
 function camelize(str) {
+  let splitStr = str.split('');
+
+  let i = 0;
+  while (splitStr[i] !== undefined) {
+    if (splitStr[i] === '-') {
+      splitStr.splice(i, 1);
+      splitStr[i] = splitStr[i].toUpperCase();
+    }
+    
+    i++;
+  }
+  
+  return splitStr.join('');
 }
