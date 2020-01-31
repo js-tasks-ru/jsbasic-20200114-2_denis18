aaf1f7ac16d1c5e743cd0bea604bc59ea040bdfc
Denis Chizh <chizhds@sibur.ru>
2020-01-31 18:50:08 +0300

    Занятие 5. Задача 1

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -3,4 +3,29 @@
  * @param {Element} table
  */
 function highlight(table) {
+  for (let body of table.tBodies) {
+    for (let row of body.rows) {
+      let age = +row.cells[1].textContent;
+      let gender = row.cells[2].textContent;
+      let available = row.cells[3].dataset.available;
+      
+      if (age < 18) {
+        row.style = 'text-decoration: line-through';
+      }
+
+      if (gender === 'm') {
+        row.classList.add('male');
+      } else if (gender === 'f') {
+        row.classList.add('female');
+      }
+      
+      if (available === undefined) {
+        row.hidden = true;
+      } else if (available === 'true') {
+        row.classList.add('available');
+      } else if (available === 'false') {
+        row.classList.add('unavailable');
+      }
+    }
+  }
 }
