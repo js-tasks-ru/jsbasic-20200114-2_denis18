84144a800f461d4b6709dc3e59dfe21fa1470725
Denis Chizh <radragonorama@gmail.com>
2020-02-08 11:20:30 +0300

    Занятие 5. Задача 2

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -18,17 +18,48 @@ function SortableTable(items) {
    * @property {Element} - обязательно свойство, которое ссылается на элемент <table>
    */
   this.el = document.createElement('table');
+  
+  /**
+   * @property {Array} - массив данных, по которым строится таблица
+   */
   this.items = items;
 
-  function render() {
-    this.el.innerHTML = '<thead><tr><td>Name</td><td>Age</td><td>Salary</td><td>City</td></tr></thead>';
+  /**
+   * @property {Array} - имена полей объекта, который предаставляет элемент массива данных
+   */
+  let _itemKeys = [];
 
-    this.el.innerHTML += '<tbody>';
-    for (let item of this.items) {
-      this.el.innerHTML += `<tr><td>${item.name}</td><td>${item.age}</td><td>${item.salary}</td><td>${item.city}</td></tr>`;
+  if (items.length > 0) {
+    for (let key in items[0]) {
+      _itemKeys.push(key);
     }
-    this.el.innerHTML += '</tbody>';
   }
+
+  /**
+   * @property {Arrya} - массив с заголовками столбцов таблицы
+   */
+  this.columnNames = ['Age', 'Name', 'Salary', 'City'];
+
+  /**
+   * Метод выполняет рендеринг таблицы
+   */
+  this.render = () => {
+    let innerHTML = "";
+    
+    innerHTML += '<thead><tr>';
+    for (let columnName of this.columnNames) {
+      innerHTML += `<td>${columnName}</td>`;
+    }
+    innerHTML += '</thead></tr>';
+
+    innerHTML += '<tbody>';
+    for (let item of this.items) {
+      innerHTML += `<tr><td>${item.name}</td><td>${item.age}</td><td>${item.salary}</td><td>${item.city}</td></tr>`;
+    }
+    innerHTML += '</tbody>';
+
+    this.el.innerHTML = innerHTML;
+  };
 
   /**
    * Метод выполняет сортировку таблицы
@@ -37,10 +68,24 @@ function SortableTable(items) {
    * @param {boolean} desc - признак того, что сортировка должна идти в обратном порядке
    */
   this.sort = (column, desc = false) => {
-    this.items.sort( (a, b) => {
-      if( )
+    this.items.sort((a, b) => {
+      let key = _itemKeys[column];
+      let x = a[key];
+      let y = b[key];
+
+      if (!desc) {
+        if (x > y) return 1;
+        if (x == y) return 0;
+        if (x < y) return -1;
+      } else {
+        if (x < y) return 1;
+        if (x == y) return 0;
+        if (x > y) return -1;
+      }
     });
+
+    this.render();
   };
 
-  render();  
+  this.render();  
 }
